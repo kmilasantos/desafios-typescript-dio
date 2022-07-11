f8904997f6e1bf3351275ba106ce7c12fea382f2
kmilasantos <csilva.santos1997@gmail.com>
2022-07-11 15:10:47 -0400

    Desafio de projeto TypeScript

---
+++ desafios/desafio-1.js
@@ -0,0 +1,13 @@
+"use strict";
+/*Como podemos rodar isso em um arquivo .ts sem causar erros?
+
+let employee = {};
+employee.code = 10;
+employee.name = "John";
+
+------------------------*/
+let employee = {
+    code: 10,
+    name: "John",
+};
+console.log(employee);
